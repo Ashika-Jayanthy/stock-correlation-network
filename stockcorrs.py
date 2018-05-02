b3dd62436ddcbcdc91fd87031cf61bc34a40d5ec
Ashika-Sita <jayanthyashika@gmail.com>
2018-05-02 14:42:01 -0500

    Second

--- stockcorrs.py
+++ stockcorrs.py
@@ -1,16 +1,40 @@
-#Given n stocks, time period and a measure, calculate the correlation network and plot the graph associated with it
+#Given n stocks, time period and a variable and distance measure, calculate the correlation network and plot the graph associated with it
 
 
 import pandas as pd
 import pandas_datareader as web
+import datetime
 
 class Stock_Correlations(object):
 
-    def __init__(self, num_stocks, symb, tp, measure):
-        self.num_stocks = num_stocks
+    def __init__(self, symb, time_start, time_end, variable, measure):
+
         self.symb = symb
-        self.tp = tp
-        self.measure = measure
+        self.time_start = pd.to_datetime(time_start)
+        self.time_end = pd.to_datetime(time_end)
+        self.variable = str(variable)
+        self.measure = str(measure)
 
     def get_data(self):
-        
+        #Get all data associated with input stocks
+        data_arrays = {}
+        for stock in self.symb:
+            stock_data = web.DataReader(stock, "quandl", start,end)
+            stock_data.index.name = 'Date'
+            stock_data.reset_index(inplace=True)
+            pd.to_datetime(stock_data['Date'])
+            data_arrays[stock] = stock_data
+            return
+
+    def filter_data(self):
+        #Filter data by time period and variable of interest
+
+
+    def correlation_matrix(self):
+        #Create correlation matrix for stocks
+
+    def measure_distance(self):
+        #Calculate pairwise distance between stocks
+
+    def visualize_network(self):
+        #Visualize the network of stocks
