caccab194d4c60456ff7ab0122a609c3cc24a943
Ashika-Sita <jayanthyashika@gmail.com>
2018-05-03 19:12:29 -0500

    edit

--- find_path.py
+++ find_path.py
@@ -6,9 +6,9 @@ def find_path(graph, start, end, path=[]):
         if start not in graph.keys():
             return path
 
-        
+
         if graph[start] not in path:
             new_start = graph[start]
             path_update = find_path(graph, new_start, end, path)
 
-        return
+        return path
